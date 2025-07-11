6c955864f10e0b0ad8fbaba75436f872df559504
Adhrit Bharath <abharath@greenhillsschool.org>
2025-07-11 13:27:42 -0400

    Added arch-ecs to library

---
+++ ECS/SystemBase.cs
@@ -0,0 +1,12 @@
+namespace MonoGameLibrary.ECS;
+
+using Arch.Core;
+using Arch.System;
+
+
+public abstract class SystemBase : BaseSystem<World, float>
+{
+    public SystemBase(World world) : base(world) {}
+
+    public abstract void PhysicsUpdate();
+}
